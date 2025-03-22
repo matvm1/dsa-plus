d7b005fbd8d542297563bdf61cd5de3a283a9f74
matvm1 <15144366+matvm1@users.noreply.github.com>
2025-03-21 23:10:37 -0400

    feat(Graph): Added adjacency map and populated in constructor from path

--- src/structs/graph/Graph.java
+++ src/structs/graph/Graph.java
@@ -4,10 +4,13 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class Graph{
     private int[][] adjMatrix;
+    private Map<Integer, Map<Integer, Integer>> adjMap;
     private int maxWeightCharLen = 0;
 
     public static void main(String[] args){
@@ -15,27 +18,20 @@ public class Graph{
         g1.print();
     }
 
-    public Graph(int[][] adjMatrix){
-        this.adjMatrix = adjMatrix;
-    }
-
-    public Graph(String pathToCSV) {
+    // Create a Graph from CSV located at path pathToCSV
+    // The CSV file should contain 4 columns: head, tail, weight, bidirectional
+    // Adjacency matrices contain weights at [head][tail] (regardless of directionality), negative weights at [tail][head] if not a bidirectional edge
+    // Adjacency maps map heads to a map of their neighbors and respective weights. Bidirectionality is represented by negative weighting
+    public Graph(String pathToCSV){
         try (BufferedReader br = new BufferedReader(new FileReader(pathToCSV))
         ){
-            int headerLen = 0;
-            int csvFields = 1;
-            int c = -1;
-            while((c = br.read()) != '\n') {
-                if (c == ',')
-                    csvFields++;
-                headerLen++;
-            }
-            c = -1;
+            while(br.read() != '\n');
             br.mark(100000);
 
             HashSet<Integer> nodes = new HashSet<Integer>();
             int numEdges = 0;
             int numNodes = 0;
+            int c = -1;
             while((c = br.read()) != -1){
                 nodes.add(c - '0');
                 br.skip(1);
@@ -46,6 +42,7 @@ public class Graph{
             numNodes = nodes.size();
 
             adjMatrix = new int[numNodes][numNodes];
+            adjMap = new HashMap<>();
 
             br.reset();
 
@@ -66,6 +63,10 @@ public class Graph{
                 adjMatrix[head][tail] = weight;
                 adjMatrix[tail][head] = weight * (bidirectional == 1 ? 1 : -1);
 
+                if(!adjMap.containsKey(head))
+                    adjMap.put(head, new HashMap<>());
+                adjMap.get(head).put(tail, weight * (bidirectional == 1 ? -1 : 1));
+
                 maxWeightCharLen = Math.max(maxWeightCharLen, countDigits(adjMatrix[tail][head]));
             }
         } catch (Exception e) {
@@ -73,6 +74,7 @@ public class Graph{
         }
     }
 
+    // Pretty print a graph in the form of an adjacency matrix
     public void print(){
         int nodes = adjMatrix.length;
         for (int[] matrix : adjMatrix) {
@@ -86,7 +88,8 @@ public class Graph{
         }
     }
 
-    public static int countDigits(int num) {
+    // Calculate the number of digits in a number
+    public static int countDigits(int num){
         return (num == 0) ? 1 : (num < 0) ? (int) (Math.log10(num * -1) + 2) : (int) (Math.log10(num) + 1);
     }
 }
