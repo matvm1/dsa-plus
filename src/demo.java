c79e7dcb77ff001ed01d09f492c95dad52449bfd
matvm1 <15144366+matvm1@users.noreply.github.com>
2025-03-22 00:45:45 -0400

    chore(): Moved main() to new class src/demo.java. Created Graph extension UndirectedGraph.java

---
+++ src/demo.java
@@ -0,0 +1,8 @@
+import structs.graph.*;
+
+public class demo{
+    public static void main(String[] args){
+        UndirectedGraph g1 = new UndirectedGraph("src/structs/graph/graph1.csv");
+        g1.print('m');
+    }
+}
