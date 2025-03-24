f9cbab23182d9060258dcbbca16aaa0c3530afb8
matvm1 <15144366+matvm1@users.noreply.github.com>
2025-03-23 22:43:17 -0400

    feat(UndirectedGraph): Can now generate Min Span Trees from a Graph. Deprecated Graph.numNodes and created HashSet nodeSet. Added initial check to UndirectedGraph.addEdgeIfAcyclic(int, int, int, int) for preseence of head or tail in nodeSet. Can now check if an UndirectedGraph has a cycle from any arbitrary node using containsCycle()

--- src/structs/graph/UndirectedGraph.java
+++ src/structs/graph/UndirectedGraph.java
@@ -6,32 +6,43 @@ public class UndirectedGraph extends Graph{
         super(pathToCSV);
     }
 
-    public Graph generateMinSpanTree(char mode) {
-        if(mode == 'k')
-            return generateMinSpanTreeKruskal();
-        //if(mode == 'p')
-        //    return generateMinSpanTreePrim();
-        return null;
+    public UndirectedGraph(int numNodes){
+        super(numNodes);
     }
 
-    private Graph generateMinSpanTreeKruskal(){
-        return null;
+    public UndirectedGraph generateMinSpanTree() {
+        return generateMinSpanTreeKruskal();
+    }
+
+    public UndirectedGraph generateMinSpanTreeKruskal(){
+        this.sortEdgeList();
+
+        UndirectedGraph tree = new UndirectedGraph(getNumNodes());
+        tree.nodeSet = nodeSet;
+
+        for(int[] edge : edgeList){
+            tree.addEdgeIfAcyclic(edge[edgeListHeadIndex], edge[edgeListTaiIndex], edge[edgeListWeightIndex], edge[edgeListBidirectionalIndex]);
+            System.out.println(tree.adjMap + " | " + tree.nodeSet);
+        }
+        return tree;
     }
 
     // Adds an edge to this graph if there are no cycles once added to the graph
     // Returns true if edge was added, false otherwise
     // May resize adjMatrix if needed. If edge is not added, adjMap will remain resized, but numNodes will reflect the # of nodes accordingly
     public boolean addEdgeIfAcyclic(int head, int tail, int weight, int bidirectional){
-        this.addEdge(head, tail, weight, bidirectional);
-        if(containsCycle(head, head, new boolean[numNodes], head)){
-            this.removeEdge(head, tail);
+        if(!nodeSet.contains(head) || !nodeSet.contains(tail)){
+            addEdge(head, tail, weight, bidirectional);
+            return true;
+        }
+
+        addEdge(head, tail, weight, bidirectional);
+        if(containsCycle(head, head, new boolean[getNumNodes()], head)){
+            removeEdge(head, tail);
 
             adjMap.get(head).remove(tail);
-            if(adjMap.get(head) == null || adjMap.get(head).isEmpty()) {
-                System.out.println("fail");
-                numNodes--;
+            if(adjMap.get(head) == null || adjMap.get(head).isEmpty())
                 adjMap.remove(head);
-            }
 
             return false;
         }
@@ -39,12 +50,22 @@ public class UndirectedGraph extends Graph{
         return true;
     }
 
+    // Detects cycles in this graph
+    // Starts from the head node of the first edge in edgeList
+        // containsCycle(int origin, int node, boolean[] visited, int prevNode) detects a cycle for a connected graph starting from any arbitrary node
+        // as long as the graph is connected
+    // Does not support forests i.e. must be connected
+    public boolean containsCycle(){
+        return containsCycle(edgeList.getFirst()[edgeListHeadIndex], edgeList.getFirst()[edgeListHeadIndex], new boolean[nodeSet.size()], edgeList.getFirst()[edgeListHeadIndex]);
+    }
+
+    // Checks to see if this graph contains a cycle. Begins checking from node node
     // DFS with matrix
     // O(V^2) - matrix representation -> Iterates over V nodes for each vertex. Worst case: graph is entirely connected
-    private boolean containsCycle(int origin, int node, boolean[] visited, int prevNode){
+    public boolean containsCycle(int origin, int node, boolean[] visited, int prevNode){
         visited[node] = true;
         // Optimized solutions use a disjoint-set (union-find) to reduce space & time complexity
-        for(int i = 0; i < adjMatrix.length; i++){
+        for(int i = 0; i < getNumNodes(); i++){
             if(adjMatrix[node][i] != 0) {
                 if(!visited[i]) {
                     if (containsCycle(origin, i, visited, node))
