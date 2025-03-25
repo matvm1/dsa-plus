109563252824a72ca8312f62e3b56aea6a6cc234
Mateo <15144366+matvm1@users.noreply.github.com>
2025-03-25 17:28:13 -0400

    feat(UnionFind): Can now create UnionFind instances with support for quick-union. Path compression is not available yet

--- src/structs/unionfind/UnionFind.java
+++ src/structs/unionfind/UnionFind.java
@@ -3,11 +3,11 @@ package structs.unionfind;
 import java.util.Arrays;
 
 public class UnionFind {
-    public int[] components;
+    private int[] components;
     private char mode;
 
     // mode:
-        // 'f' - quick-find, eager (pre-compute components structure w/root during
+        // 'f' - quick-find, eager (pre-compute components structure w/find during
         //                          union)
         // 'u' - quick-union, lazy (components trees are flattened only when a tree
         //                          becomes too large)
@@ -23,10 +23,18 @@ public class UnionFind {
             components[i] = i;
     }
 
-    // Quick-Find: O(n)
-        // O(n^2) to process n union operations
+    // Quick-Find: Theta(n)
+        // O(n^2) to process n union commands
+    // Quick-Union:
+        // O(n)
+            // O(n^2) in worst case (no path compression, linear tree generated w/ all
+                // elements connected) to process n union commands
+        // Omega(log n)
+            // O(n log n) in the best case (unions result in a complete binary tree)
+                // to process n union commands
+        // TODO: path compression
     public void union(int p, int q) {
-        if (mode == 'f'){
+        if (mode == 'f') {
             if(components[p] == components[q])
                 return;
 
@@ -36,19 +44,48 @@ public class UnionFind {
                 if (components[i] == mergedComponent)
                     components[i] = components[q];
             }
+
+            return;
         }
+
+        if (mode == 'u') {
+            while (components[p] != p && components[p] != q)
+                p = components[p];
+
+            // check if they aren't connected already
+            if(components[p] != q)
+                components[p] = find(q);
+
+            return;
+        }
+
+        throw new RuntimeException("UnionFind mode not set for object " + System.identityHashCode(this));
     }
 
     // Quick-Find: O(1)
+    // Quick-Union: O(n)
     public boolean connected(int p, int q) {
-        if(mode == 'f')
+        if (mode == 'f')
             return components[p] == components[q];
 
-        // TODO
-        else return false;
+        if (mode == 'u') {
+            while (components[p] != p && components[p] != q)
+                p = components[p];
+
+            // check if they are connected before the root
+            if(components[p] == q)
+                return true;
+
+            return p == find(q);
+        }
+
+        throw new RuntimeException("UnionFind mode not set for object " + System.identityHashCode(this));
     }
 
-    public int root(int p) {
+    // Returns the root of p
+    // O(n) if p is the leaf node in a tree where all components are connected linearly
+        // TODO: Path compression for faster search
+    public int find(int p) {
         if (mode == 'f')
             return components[p];
 
