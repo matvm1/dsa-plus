0671158308c70d9050f3892b197851efcdb416b5
matvm1 <15144366+matvm1@users.noreply.github.com>
2025-04-03 17:21:36 -0400

    feat(LinkedList): Created LinkedList library with operations for initializing from an oject array, adding elements, getting the size, and getting the head/tail references

--- src/structs/collections/Main.java
+++ src/structs/collections/Main.java
@@ -4,5 +4,12 @@ public class Main {
     public static void main(String[] args) {
         ListNode<String> n = new ListNode<>("Hello");
         System.out.println(n);
+
+        String[] strList = {"Hello", "Java", "World", "My", "Name", "Is", "Mateo"};
+        LinkedList<String> l1 = new LinkedList<>(strList);
+        System.out.println(l1);
+        System.out.println(l1.getHead());
+        System.out.println(l1.getTail());
+        System.out.println(l1.size());
     }
 }
