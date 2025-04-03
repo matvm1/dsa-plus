638e9af7d2ceba46167ce86b21bb09969563a8ea
Mateo <15144366+matvm1@users.noreply.github.com>
2025-04-03 17:21:36 -0400

    feat(LinkedList): Created LinkedList library with operations for initializing from an oject array, adding elements, getting the size, and getting the head/tail references

--- src/structs/collections/LinkedList.java
+++ src/structs/collections/LinkedList.java
@@ -1,16 +1,36 @@
 package structs.collections;
 
+import java.util.Arrays;
+
 public class LinkedList<T> implements List<T> {
     private int size;
+    private ListNode<T> head;
+    private ListNode<T> tail;
 
     public LinkedList(T[] arr) {
-        size = arr.length;
-        // TODO
+        for (T data: arr) {
+            if (!add(data))
+                throw new IllegalStateException("Failed to add the following data: " + data);
+        }
     }
 
     @Override
     public boolean add(T data) {
-        return false;
+        ListNode<T> newNode = new ListNode<>(data);
+        size++;
+
+        if (head == null) {
+            head = newNode;
+            head.next = tail;
+            tail = head;
+            return true;
+        }
+
+        assert tail != null;
+        tail.next = newNode;
+        tail = newNode;
+
+        return true;
     }
 
     @Override
@@ -25,11 +45,33 @@ public class LinkedList<T> implements List<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0 && head == null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+
+        ListNode<T> tmp = head;
+        while (tmp.next != null) {
+            sb.append(tmp.data.toString()).append(", ");
+            tmp = tmp.next;
+        }
+        sb.append(tmp.data.toString()).append("]");
+
+        return sb.toString();
+    }
+
+    public ListNode<T> getHead() {
+        return head;
+    }
+
+    public ListNode<T> getTail() {
+        return tail;
     }
 }
