0d1f3164ec0ea4dafbf306b000b116780429b5b7
matvm1 <15144366+matvm1@users.noreply.github.com>
2025-05-13 16:39:21 -0400

    feat(BST): Now tracking tree sizes

--- src/structs/collections/BSTMap.java
+++ src/structs/collections/BSTMap.java
@@ -15,6 +15,7 @@ public class BSTMap<Key extends Comparable<Key>, Value> {
         Node<Key, Value> curr = root;
 
         while (curr != null) {
+            curr.size++;
             prev = curr;
             int cmp = key.compareTo(curr.key);
             if (cmp < 0)
@@ -28,34 +29,54 @@ public class BSTMap<Key extends Comparable<Key>, Value> {
         }
 
         if (curr == null) {
-            if (prev != null)
+            if (prev != null) {
                 if (key.compareTo(prev.key) < 0)
                     prev.left = new Node<>(key, value);
                 else
                     prev.right = new Node<>(key, value);
+            }
             else
                 root = new Node<>(key, value);
+        }
+        // no insertion, undo size increments
+        else {
+            curr = root;
+            do {
+                curr.size--;
+                int cmp = key.compareTo(curr.key);
+                if (cmp < 0)
+                    curr = curr.left;
+                else if (cmp > 0)
+                    curr = curr.right;
+            } while (curr.key != key);
+            curr.size--;
         }
 
         assert(isBST());
     }
 
-    public String toString() {
-        return toString(root, new StringBuilder(), 0).toString();
+    private static <Key extends Comparable<Key>, Value> int size(Node<Key, Value> node) {
+        return (node == null) ? 0 : node.size;
     }
 
-    private StringBuilder toString(Node<Key, Value> node, StringBuilder sb,
+    public String toString() {
+        return toStringBuilder(root, new StringBuilder(), 0).toString();
+    }
+
+    private StringBuilder toStringBuilder(Node<Key, Value> node, StringBuilder sb,
                             int recursionLevel) {
         if (node == null)
             return sb;
 
-        toString(node.left, sb, recursionLevel + 1);
+        toStringBuilder(node.left, sb, recursionLevel + 1);
         sb.append("-".repeat(recursionLevel))
                 .append("[")
                 .append(node.key)
-                .append("]")
+                .append("](")
+                .append(node.size)
+                .append(")")
                 .append(node.value).append("\n");
-        toString(node.right, sb, recursionLevel + 1);
+        toStringBuilder(node.right, sb, recursionLevel + 1);
 
         return sb;
     }
@@ -78,10 +99,16 @@ public class BSTMap<Key extends Comparable<Key>, Value> {
         private Node<Key, Value> right;
         private final Key key;
         private Value value;
+        private int size;
 
         private Node(Key key, Value value) {
             this.key = key;
             this.value = value;
+            size = 1;
+        }
+
+        public String toString() {
+            return "[" + key + "]" + value;
         }
     }
 }
